0412826c8e0695e96b776297df30f32d4ccc055c
Marques Lee <marques.lee@thoughtworks.com>
2016-07-06 12:35:13 -0700

    safer file exchange command assembly

--- lib/lxc/file_exchange.rb
+++ lib/lxc/file_exchange.rb
@@ -1,15 +1,24 @@
 class LXC
   module FileExchange
+
     # may refactor copy file and read file to use streams so we can deal with large files
     def copy_from_host_to_container(name, source_path_on_host, target_path_on_container)
       info "[LXC] COPY TO CONTAINER #{name}: #{source_path_on_host} => #{target_path_on_container}"
-      exec "lxc-attach -n #{name} -- mkdir -p #{Shellwords.escape(File.dirname(target_path_on_container))}"
-      exec "cat #{Shellwords.escape(source_path_on_host)} | lxc-attach -n #{name} -- /bin/sh -c #{Shellwords.escape("/bin/cat > " + Shellwords.escape(target_path_on_container))}"
+
+      ensure_directory_exists_on_container = Shellwords.join(["lxc-attach", "-n", name, "--", "mkdir", "-p", File.dirname(target_path_on_container)])
+      stream_data_to_container = [
+        Shellwords.join(["cat", source_path_on_host]),
+        Shellwords.join(["lxc-attach", "-n", name, "--", "/bin/sh", "-c", "/bin/cat > " + Shellwords.escape(target_path_on_container)])
+      ].join(" | ")
+
+      exec ensure_directory_exists_on_container
+      exec stream_data_to_container
     end
 
     def read_file_from_container(name, path_on_container)
       info "[LXC] READ FILE IN CONTAINER #{name}: #{path_on_container}"
-      exec "lxc-attach -n #{name} -- /bin/cat #{Shellwords.escape path_on_container}"
+      exec Shellwords.join(["lxc-attach", "-n", name, "--", "/bin/cat", path_on_container])
     end
+
   end
 end
